fff930fd916fa7220a6b01f0febba32aa62c8495
mzt5441 <mzt5441@IGM2000-26>
2018-02-16 17:48:15 -0500

    Finished the milestone 1!!!

--- A_TNT_Maker/A_TNT_Maker/Header.h
+++ A_TNT_Maker/A_TNT_Maker/Header.h
@@ -1,10 +1,10 @@
 #pragma once
 
-char* GetTeam();
-void SetMaze(const int** data, int length);
-int** GetMaze();
-void GetNextPosition(int& xpos, int& ypos);
-void SetStart(int xpos, int ypos);
-void GetStart(int& xpos, int& ypos);
-void SetEnd(int xpos, int ypos);
-void GetEnd(int& xpos, int& ypos);
+_declspec(dllexport) char* GetTeam();
+_declspec(dllexport) void SetMaze(const int** data, int width, int height);
+_declspec(dllexport) int** GetMaze(int& width, int& height);
+_declspec(dllexport) void GetNextPosition(int& xpos, int& ypos);
+_declspec(dllexport) void SetStart(int xpos, int ypos);
+_declspec(dllexport) void GetStart(int& xpos, int& ypos);
+_declspec(dllexport) void SetEnd(int xpos, int ypos);
+_declspec(dllexport) void GetEnd(int& xpos, int& ypos);
