18ce62ce6298f8e992829827d4869371c4fb73fe
mzt5441 <mzt5441@IGM2435-25>
2018-03-28 12:12:22 -0400

    Worked on unit tests

--- A_TNT_Maker/UnitTests/unittest.cpp
+++ A_TNT_Maker/UnitTests/unittest.cpp
@@ -10,10 +10,39 @@ namespace UnitTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(SetMaze)
 		{
 			// TODO: Your test code here
 		}
 
+		TEST_METHOD(GetMaze)
+		{
+
+		}
+
+		TEST_METHOD(GetNextPosition)
+		{
+
+		}
+
+		TEST_METHOD(SetStart)
+		{
+
+		}
+
+		TEST_METHOD(GetStart)
+		{
+
+		}
+
+		TEST_METHOD(SetEnd)
+		{
+
+		}
+
+		TEST_METHOD(GetEnd)
+		{
+
+		}
 	};
 }
