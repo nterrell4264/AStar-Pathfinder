c0a197776fae727edf0eb114a145248f19c277de
nat7611 <nat7611@g.rit.edu>
2018-03-29 22:10:19 -0400

    Too tired to remember what changes were made.

--- A_TNT_Maker/A_TNT_Maker/Graph.cpp
+++ A_TNT_Maker/A_TNT_Maker/Graph.cpp
@@ -24,18 +24,18 @@ Graph::Graph(int*** maze, int width, int height)
 
 Graph::~Graph()
 {
-	delete[] start;
+	/*delete[] start;
 	delete[] end;
 
 	for (int i = 0; i < pathLength; i++) {
 		delete (*path)[i];
-	}
+	}*/
 }
 
 bool Graph::CalculatePath(int x, int y)
 {
 	Vertex* vertex = matrix[x][y];
-	if (*mazeP[x][y] == 0 || (*vertex).visited) return false;
+	if (*mazeP[x][y] != 0 || (*vertex).visited) return false;
 	if (x == *end && y == *(end + 1)) return true; //Reached end
 	//Add vertex to path
 	(*path).push_back(vertex);
@@ -54,6 +54,7 @@ bool Graph::CalculatePath(int x, int y)
 	if (*((*vertex).adjacencies + 3)) {
 		if (CalculatePath(x, y - 1)) return true;
 	}
+	//Removes vertex from path if there is no path from it
 	(*path).pop_back();
 	pathLength--;
 	return false;
