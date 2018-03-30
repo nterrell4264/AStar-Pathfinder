e71d7966b89b7ea22ccaa550dfe3cbf2480ec9d0
nat7611 <nat7611@g.rit.edu>
2018-03-30 18:33:19 -0400

    It works! PogChamp

--- A_TNT_Maker/A_TNT_Maker/Graph.cpp
+++ A_TNT_Maker/A_TNT_Maker/Graph.cpp
@@ -12,11 +12,14 @@ Graph::Graph(int*** maze, int width, int height)
 		for (int x = 0; x < width; x++) {
 			matrix[x] = new Vertex*[height];
 			for (int y = 0; y < height; y++) {
-				matrix[x][y] = new Vertex(x, y);
-				if (x < width - 1 && (*maze)[x + 1][y] == 0) matrix[x][y]->AddAdjacency(0);
-				if (y < height - 1 && (*maze)[x][y + 1] == 0) matrix[x][y]->AddAdjacency(1);
-				if (x > 0 && (*maze)[x - 1][y] == 0) matrix[x][y]->AddAdjacency(2);
-				if (y > 0 && (*maze)[x][y - 1] == 0) matrix[x][y]->AddAdjacency(3);
+				Vertex* vertex = new Vertex(x, y);
+				if (x < width - 1 && (*maze)[x + 1][y] == 0) vertex->AddAdjacency(0);
+				if (y < height - 1 && (*maze)[x][y + 1] == 0) vertex->AddAdjacency(1);
+				if (x > 0 && (*maze)[x - 1][y] == 0) vertex->AddAdjacency(2);
+				if (y > 0 && (*maze)[x][y - 1] == 0) vertex->AddAdjacency(3);
+				vertex->startDis = abs(x - *start) + abs(y - *(start + 1));
+				vertex->endDis = abs(x - *end) + abs(y - *(end + 1));
+				matrix[x][y] = vertex;
 		}
 	}
 	path = new vector<Vertex*>[1];
@@ -36,13 +39,13 @@ bool Graph::CalculatePath(int x, int y)
 {
 	Vertex* vertex = matrix[x][y];
 	if ((*mazeP)[x][y] != 0 || (*vertex).visited) return false;
-	if (x == *end && y == *(end + 1)) return true; //Reached end
 	//Add vertex to path
 	(*path).push_back(vertex);
 	pathLength++;
+	if (x == *end && y == *(end + 1)) return true; //Reached end
 	(*vertex).visited = true;
 	//Recursively finds next step
-	if ((*vertex).adjacencies) {
+	if (*((*vertex).adjacencies)) {
 		if (CalculatePath(x + 1, y)) return true;
 	}
 	if (*((*vertex).adjacencies + 1)) {
